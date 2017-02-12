df2461d2ee3f8b6877bf8cbdf053d3b28681e0e7
onefen <ivo.malakov85@gmail.com>
2017-02-12 16:18:11 +0200

    New Exercise added - Create a SharpStore website using the handmade HTTP server (see lab 4 - workshop "Handmade HTTP Server"). I made the first 4 tasks and 4 more left.

---
+++ Exercise 4 - Sharp Store with Handmade HTTP Server/SharpStore/SharpStore/SharpStore.cs
@@ -0,0 +1,143 @@
+﻿using SimpleHttpServer.Models;
+using System.Collections.Generic;
+using System.IO;
+using SimpleHttpServer;
+
+namespace SharpStore
+{
+    class SharpStore
+    {
+        static void Main(string[] args)
+        {
+            var routes = new List<Route>()
+            {
+                new Route()
+                {
+                    Name = "Home Directory",
+                    Method = SimpleHttpServer.Enums.RequestMethod.GET,
+                    UrlRegex = "^/home$",
+                    Callable = (request) =>
+                    {
+                        return new HttpResponse()
+                        {
+                            StatusCode = SimpleHttpServer.Enums.ResponseStatusCode.Ok,
+                            ContentAsUTF8 = File.ReadAllText("../../content/home.html")
+                        };
+                    }
+                },
+                new Route()
+                {
+                    Name = "About Directory",
+                    Method = SimpleHttpServer.Enums.RequestMethod.GET,
+                    UrlRegex = "^/about$",
+                    Callable = (request) =>
+                    {
+                        return new HttpResponse()
+                        {
+                            StatusCode = SimpleHttpServer.Enums.ResponseStatusCode.Ok,
+                            ContentAsUTF8 = File.ReadAllText("../../content/about.html")
+                        };
+                    }
+                },
+                new Route()
+                {
+                    Name = "Product Directory",
+                    Method = SimpleHttpServer.Enums.RequestMethod.GET,
+                    UrlRegex = "^/products$",
+                    Callable = (request) =>
+                    {
+                        return new HttpResponse()
+                        {
+                            StatusCode = SimpleHttpServer.Enums.ResponseStatusCode.Ok,
+                            ContentAsUTF8 = File.ReadAllText("../../content/products.html")
+                        };
+                    }
+                },
+                new Route()
+                {
+                    Name = "Contact Directory",
+                    Method = SimpleHttpServer.Enums.RequestMethod.GET,
+                    UrlRegex = "^/contacts$",
+                    Callable = (request) =>
+                    {
+                        return new HttpResponse()
+                        {
+                            StatusCode = SimpleHttpServer.Enums.ResponseStatusCode.Ok,
+                            ContentAsUTF8 = File.ReadAllText("../../content/contacts.html")
+                        };
+                    }
+                },
+                new Route()
+                {
+                    Name = "Images",
+                    Method = SimpleHttpServer.Enums.RequestMethod.GET,
+                    UrlRegex = @"^/images/.+",
+                    Callable = (request) =>
+                    {
+                        var nameOfFile = request.Url.Substring(request.Url.LastIndexOf('/') + 1);
+                        var response = new HttpResponse()
+                        {
+                            StatusCode = SimpleHttpServer.Enums.ResponseStatusCode.Ok,
+                            Content = File.ReadAllBytes($"../../content/images/{nameOfFile}")
+                        };
+                        response.Header.ContentType = "images/*";
+                        response.Header.ContentLength = response.Content.Length.ToString();
+
+                        return response;
+                    }
+                },
+                new Route()
+                {
+                    Name = "Carousel CSS",
+                    Method = SimpleHttpServer.Enums.RequestMethod.GET,
+                    UrlRegex = "^/content/css/carousel.css$",
+                    Callable = (request) =>
+                    {
+                        var response = new HttpResponse()
+                        {
+                            StatusCode = SimpleHttpServer.Enums.ResponseStatusCode.Ok,
+                            ContentAsUTF8 = File.ReadAllText("../../content/css/carousel.css")
+                        };
+                        response.Header.ContentType = "text/css";
+                        return response;
+                    }
+                },
+                new Route()
+                {
+                    Name = "Bootstrap JS",
+                    Method = SimpleHttpServer.Enums.RequestMethod.GET,
+                    UrlRegex = "^/bootstrap/js/bootstrap.min.js$",
+                    Callable = (request) =>
+                    {
+                        var response = new HttpResponse()
+                        {
+                            StatusCode = SimpleHttpServer.Enums.ResponseStatusCode.Ok,
+                            ContentAsUTF8 = File.ReadAllText("../../content/bootstrap/js/bootstrap.min.js")
+                        };
+                        response.Header.ContentType = "application/x-javascript";
+                        return response;
+                    }
+                },
+                new Route()
+                {
+                    Name = "Bootstrap CSS",
+                    Method = SimpleHttpServer.Enums.RequestMethod.GET,
+                    UrlRegex = "^/bootstrap/css/bootstrap.min.css$",
+                    Callable = (request) =>
+                    {
+                        var response = new HttpResponse()
+                        {
+                            StatusCode = SimpleHttpServer.Enums.ResponseStatusCode.Ok,
+                            ContentAsUTF8 = File.ReadAllText("../../content/bootstrap/css/bootstrap.min.css")
+                        };
+                        response.Header.ContentType = "text/css";
+                        return response;
+                    }
+                }
+            };
+
+            SimpleHttpServer.HttpServer httpServer = new HttpServer(8081, routes);
+            httpServer.Listen();
+        }
+    }
+}
